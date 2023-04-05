8cee623727da53601a3b1b1a9bff5305480c80cb
Soviro <svrverstraeten@gmail.com>
2023-04-05 10:45:28 +0200

    commit age v2

--- age.js
+++ age.js
@@ -5,8 +5,10 @@ console.log('What\'s your age?')
 
 process.stdin.on('data', (age) => {
 
-    if (age>0 && age<100 && age!=2023) {
+    if (age>0 && age<100) {
     console.log('Your year of birth is: ' + (2023-age)); 
+} else {
+    console.log('Age needs to be between 0 adn 100');
 }
     process.exit()
 })
